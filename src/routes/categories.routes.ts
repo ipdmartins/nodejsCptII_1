814169892c26a85584d65052bb0708553473e774
ipdmartins <ipdmartins@gmail.com>
2022-02-01 23:25:22 -0500

    Remove the now ignored directory node_modules

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,29 @@
-import {Request, Response, Router } from 'express';
-import CreateCourseService from './CreateCourseService';
+import { Request, Response, Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+// import CreateCourseService from './CreateCourseService';
 
 const categoriesRoutes = Router();
 
-categoriesRoutes.post('/categories', (request, response) => {
-  
-}){
-  CreateCourseService.execute({
-    name: 'Nodejs',
-    duration: 10,
-    educator: 'Dani'
+const categories = [];
+
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+  categories.push({
+    name,
+    description,
+    id: uuidv4(),
   });
+  return response.send(201);
+});
+// categoriesRoutes.post('/categories', (request, response) => {
+// }){
+//   CreateCourseService.execute({
+//     name: 'Nodejs',
+//     duration: 10,
+//     educator: 'Dani'
+//   });
 
-  return response.send(200);
-}
+//   return response.send(200);
+// }
 
-export {categoriesRoutes};
+export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express, { response } from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/courses', (request, response) => {
-  const { name } = request.body;
-  return response.json({ name })
-})
+app.use('/categories', categoriesRoutes);
+
+// app.post('/courses', (request, response) => {
+//   const { name } = request.body;
+//   return response.json({ name })
+// })
 
 app.listen(3333, () => console.log('Server is running on port 3333'));
