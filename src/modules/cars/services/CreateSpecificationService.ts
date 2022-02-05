b0d97d0d3bed68dfaebf84eef3042ddccacf9ba6
ipdmartins <ipdmartins@gmail.com>
2022-02-04 23:45:28 -0500

    feature: implement SOLID (the S and L principle

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,22 @@
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  constructor(private categoriesRepository: ICategoriesRepository){}
+
+  execute({ name, description }: ICreateCategoryDTO): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error('Category already exists');
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateSpecificationService };
