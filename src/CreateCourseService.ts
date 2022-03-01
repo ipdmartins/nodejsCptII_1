6810d7c935140ff35f01d408ca02ca5bab75bfd3
Igor Martins <ipdmartins@gmail.com>
2022-02-28 23:38:40 -0500

    updates

--- src/CreateCourseService.ts
+++ src/CreateCourseService.ts
@@ -7,6 +7,7 @@ interface Course {
 class CreateCourseService {
   //com esta sintaxe, a duration foi definida com um valor default de 8
   execute({ name, duration = 8, educator }: Course) {
+
     console.log(name, duration, educator);
   }
 }
